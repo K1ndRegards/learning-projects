176812e8e21d0ba92155c3909049839423e5f342
K1ndRegards <kindreg4rds@gmail.com>
2026-07-06 15:20:41 +0300

    refactor(interactions): change approach in display-fade animation

--- components/dynamic-components/interactions/fade-toggle-v1/src/main.js
+++ components/dynamic-components/interactions/fade-toggle-v1/src/main.js
@@ -2,39 +2,12 @@ function toggleElement(el) {
   el.classList.toggle('hidden');
 }
 
-function fadeIn(el) {
-  toggleElement(el);
-  el.classList.add('fade');
-
-  requestAnimationFrame(() => {
-    el.classList.add('fade--visible');
-  });
-}
-
-function fadeOut(el) {
-  el.classList.remove('fade--visible');
-
-  el.addEventListener('transitionend', function handler() {
-    toggleElement(el);
-    el.removeEventListener('transitionend', handler);
-  });
-}
-
-function toggleFade(el) {
-  const isVisible = !Array.from(el.classList).includes('hidden');
-
-  if (isVisible) {
-    fadeOut(el);
-  } else {
-    fadeIn(el);
-  }
-}
-
 const btn = document.querySelector('.animation__btn');
-const items = Array.from(document.querySelector('.animation__list').children);
+const itemList = document.querySelector('.animation__list');
+const listItems = itemList.querySelectorAll('.animation__item');
 
-Array.from(items).forEach((item) => item.classList.add('hidden'));
+listItems.forEach((item) => item.classList.add('hidden'));
 
 btn.addEventListener('click', function () {
-  items.forEach((item) => toggleFade(item));
+  listItems.forEach((item) => toggleElement(item));
 });
